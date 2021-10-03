7c6428bd39f8e4d687649cc4f2a72c81c391f9ff
Irwin Mier <mierirwinjan@gmail.com>
2021-10-03 15:48:17 -0700

    Update sketch.js
    new constrain features, globalizing variables, new functions outside of mouseDragged(), new text and buttons for user interaction.

--- project2/sketch.js
+++ project2/sketch.js
@@ -9,8 +9,13 @@ panFromY = 0;
 panToX = 0;
 panToY = 0;
 
+leftWall = 0; //boundary of left side of canvas
+rightWall = 600; //boundary of right side of original image size
+upperWall = 0; //boundary for upper side of canvas
+bottomWall = 450; //boundary for bottom side of canvas
 
-function preload(){
+
+function preload() { 
   img = loadImage('data/Campus100dpi.png'); //satellite
   imgBldgs = loadImage('data/Buildings100dpi.png'); //building shapes
   tblBldgs = loadTable('data/Building_Codes_updated.csv', 'csv', 'header');
@@ -28,16 +33,20 @@ function draw() {
   mainImg = image(img, imgX, imgY, imgW, imgH); //larger image in main screen
   bldgImg = image(img, imgX, imgY, imgW, imgH); //building map (added)
   overviewImg = image(img, 600, 0, 200, (200 * img.height) / img.width); //smaller image
-  introScreen()
   
+  introScreen();
+  ifSmallImage();
+  ifLargerImage();
+  originalSize();
 }
 
-function introScreen(){
+function introScreen() {
   rect1 = rect(600,120,200,450);
   //fill(115);
   textSize(15);
   textFont('Helvetica');  
-  text("Refillable Water Stations of SDSU: Click and hover on any part of the map", 610, 150, 170, 170);
+  text("Refillable Water Stations SDSU: Click and hover over any part of the map", 610, 140, 170, 170);
+  text("Press 'P' to zoom in, 'M' to zoom out, and 'R' to reset size", 610, 220, 170, 170);
   mousePressed(); //returns name of building if hovered over
   //keyPressed();  // will zoom in continuously if pressed
 }
@@ -70,16 +79,6 @@ function mousePressed() {
 }
 
 function mouseDragged() {
-
-  
-  leftWall = 0; //boundary of left side of canvas
-  rightWall = 600; //boundary of right side of original image size
-  upperWall = 0; //boundary for upper side of canvas
-  bottomWall = 450; //boundary for bottom side of canvas
-  
-  originalFramex = rightWall - leftWall;
-  originalFramey = bottomWall - upperWall;
-  
   panToX = mouseX;
   panToY = mouseY;
   xShift = panToX - panFromX;
@@ -94,20 +93,35 @@ function mouseDragged() {
   pamFromX = panToX;
   panFromY = panToY;
   
-  //constrain original sized image in default position
+  originalSize();
+  ifLargerImage();
+}
+
+function originalSize() {  
+  //constrain original sized image in default position, keep so that theres no gap between blank space and image when it resets
   if ((imgW == rightWall) && (imgH == bottomWall) || (imgW < rightWall) || (imgH < bottomWall)) { 
     imgX = constrain(imgX, leftWall, rightWall - imgW);
     imgY = constrain(imgY, upperWall, bottomWall  - imgH);
-  }
+  }    
+}
+
+function ifLargerImage() { 
+  originalFramex = rightWall - leftWall;
+  originalFramey = bottomWall - upperWall;
   
   //make new conditions if image size is bigger than size of original screen
-  if ((imgW > rightWall) || (imgH > bottomWall)) {
+   if ((imgW > rightWall) || (imgH > bottomWall)) {
     imgX = constrain(imgX, -(imgW - originalFramex), -(imgW - originalFramex) + (imgW - rightWall));
     imgY = constrain(imgY, -(imgH - originalFramey), -(imgH - originalFramey) + (imgH - bottomWall));
   } 
-  //this constrains the enlarged image within the canvas if the borders of the image touch the borders of the canvas size
+}
 
-  
+function ifSmallImage() {  
+  // resets the photo to original size if user tries to make image smaller than screen size
+  if ((imgW < rightWall) || (imgH < bottomWall)) {
+    imgW = rightWall;
+    imgH = bottomWall;
+  }  
 }
 
 function keyPressed() {
@@ -129,6 +143,12 @@ function keyPressed() {
     imgW = int(imgW * (1 - scaleFactor));
     imgH = int(imgH * (1 - scaleFactor));
   }
+  
+  //reset to original size by clicking a button
+  if (key == "r"){
+    imgW = rightWall;
+    imgH = bottomWall;
+  }
 }
 
 function mouseWheel(event) {
@@ -142,4 +162,3 @@ function mouseWheel(event) {
 function windowResize() {
   resizeCanvas(windowWidth, windowHeight);
 }
-
